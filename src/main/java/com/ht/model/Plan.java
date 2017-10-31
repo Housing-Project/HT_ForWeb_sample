5963d83a9c715d4b99ccdcb064ea7b9fe1844305
ruarua <2392981127@qq.com>
2017-10-31 20:35:02 +0800

    commit model

--- src/main/java/com/ht/model/Plan.java
+++ src/main/java/com/ht/model/Plan.java
@@ -1,59 +1,53 @@
 package com.ht.model;
 
 public class Plan {
-    
-   private int planId;
-   private String planName;
-   private int planFloor;
-   private int planArea;
-   private int planStatus;
-   private String planPicPath;
-   
-   public Plan(){
-	   
-   }
-   public int getPlanId() {
-	   return planId;
-   }
-   public void setPlanId(int planId) {
-	   this.planId = planId;
-   }
-   public String getPlanName() {
-	   return planName;
-   }
-   public void setPlanName(String planName) {
-	   this.planName = planName;
-   }
-   public int getPlanFloor() {
-	   return planFloor;
-   }
-   public void setPlanFloor(int planFloor) {
-	   this.planFloor = planFloor;
-   }
-   public int getPlanArea() {
-	   return planArea;
-   }
-   public void setPlanArea(int planArea) {
-	   this.planArea = planArea;
-   }
-   public int getPlanStatus() {
-	   return planStatus;
-   }
-   public void setPlanStatus(int planStatus) {
-	   this.planStatus = planStatus;
-   }
-   public String getPlanPicPath() {
-	   return planPicPath;
-   }
-   public void setPlanPicPath(String planPicPath) {
-	   this.planPicPath = planPicPath;
-   }
-@Override
-public String toString() {
-	return "Plan [planId=" + planId + ", planName=" + planName + ", planFloor=" + planFloor + ", planArea=" + planArea
-			+ ", planStatus=" + planStatus + ", planPicPath=" + planPicPath + "]";
-}
-  
-   
-   
+
+	private int planId;
+	private int planFloor;
+	private int planMinArea;
+	private int planMaxArea;
+	private String planPicPath;
+	private int planStatus;
+	public int getPlanId() {
+		return planId;
+	}
+	public void setPlanId(int planId) {
+		this.planId = planId;
+	}
+	public int getPlanFloor() {
+		return planFloor;
+	}
+	public void setPlanFloor(int planFloor) {
+		this.planFloor = planFloor;
+	}
+	public int getPlanMinArea() {
+		return planMinArea;
+	}
+	public void setPlanMinArea(int planMinArea) {
+		this.planMinArea = planMinArea;
+	}
+	public int getPlanMaxArea() {
+		return planMaxArea;
+	}
+	public void setPlanMaxArea(int planMaxArea) {
+		this.planMaxArea = planMaxArea;
+	}
+	public String getPlanPicPath() {
+		return planPicPath;
+	}
+	public void setPlanPicPath(String planPicPath) {
+		this.planPicPath = planPicPath;
+	}
+	public int getPlanStatus() {
+		return planStatus;
+	}
+	public void setPlanStatus(int planStatus) {
+		this.planStatus = planStatus;
+	}
+	@Override
+	public String toString() {
+		return "Plan [planId=" + planId + ", planFloor=" + planFloor + ", planMinArea=" + planMinArea + ", planMaxArea="
+				+ planMaxArea + ", planPicPath=" + planPicPath + ", planStatus=" + planStatus + "]";
+	}
+	
 }
